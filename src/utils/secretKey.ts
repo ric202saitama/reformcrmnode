38376ce851cf546e0ad30452ffb2f9b3627acd52
リカルド <ric202cjc@gmail.com>
2025-02-23 13:50:14 +0900

    added customermaster

--- src/utils/secretKey.ts
+++ src/utils/secretKey.ts
@@ -1,4 +1,4 @@
 export const WEBTOKEN_SECRET_KEY = "5a1f4b3e2c8d7a6f9e1d2c3b4a5f6e7d8c9a0b1f2e3d4c5b6a7f8e9d0c1a2b3";
 export const GOOGLE_EMAIL_USER = "rifomusapoto@gmail.com";
-export const GOOGLE_APP_PASSOWRD="qivk epeu rlhh wpgv";
+export const GOOGLE_APP_PASSOWRD="qivk";
 export const MAIL_PASSWORD_URL="http://localhost:5173/security";
